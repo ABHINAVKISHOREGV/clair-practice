cb0bc90717cc0eb6016e452371abfac910bc00a4
AbhinavkishoreGV <64400833+ABHINAVKISHOREGV@users.noreply.github.com>
2020-05-09 23:12:27 +0530

    Create res.py

---
+++ res.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import json
+
+def write_json_file(filename):
+    with open(filename, "r") as f:
+        data= json.load(f)
+    for i in range(len(data[0])-1):
+    	print("Name of the image: ",data[i]["image"])
+    	for j in data[i]["vulnerabilities"]:
+    		if j["severity"]=="Critical" or j["severity"]=="High":
+    			for k,l in j.items():
+    				print(k," : ",l)
+    			print()
+    	print()
+if __name__ == "__main__":
+    data=''
+    write_json_file('package.json')
